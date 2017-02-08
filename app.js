74f220171b9e3fa2167f6108257fb5e20b2179c6
Krish Rajagopalan <krajagopalan@doximity.com>
2017-02-07 21:18:23 -0800

    Allow firstname and lastname to be dynamically set

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var g = G$("Homer", "Simpson");
 g.greet().setLang('es').greet(true).log();
 
 $("#login").on("click", () => {
-  var loginGrtr = G$("Randy", "Marsh");
+  var firstName = $("#firstname").val();
+  var lastName = $("#lastname").val();
+  var language = $("#lang").val();
+
+  var loginGrtr = G$(firstName, lastName);
   $("#logindiv").hide();
-  loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
+  loginGrtr.setLang(language).HTMLGreeting("#greeting", true).log();
 });
