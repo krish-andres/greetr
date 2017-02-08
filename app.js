62183400114e917b5d780143a996f17ba56cd222
Krish Rajagopalan <krajagopalan@doximity.com>
2017-02-07 21:19:46 -0800

    Change button text/id to make more sense

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var g = G$("Homer", "Simpson");
 g.greet().setLang('es').greet(true).log();
 
-$("#login").on("click", () => {
+$("#greet").on("click", () => {
   var firstName = $("#firstname").val();
   var lastName = $("#lastname").val();
   var language = $("#lang").val();
 
   var loginGrtr = G$(firstName, lastName);
-  $("#logindiv").hide();
+  $("#greetingdiv").hide();
   loginGrtr.setLang(language).HTMLGreeting("#greeting", true).log();
 });
